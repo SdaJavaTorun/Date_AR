97109609264b35db2c5a3d3a299d883dca645d7b
grovas <arczir@gmail.com>
2017-04-11 20:58:53 +0200

    2nd comit

--- src/main/java/com/sdajava/data/Main.java
+++ src/main/java/com/sdajava/data/Main.java
@@ -2,6 +2,9 @@ package com.sdajava.data;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
+import java.util.Set;
 
 public class Main {
 
@@ -13,8 +16,17 @@ public class Main {
 
         System.out.println(time);
 
-        LocalTime teenClock = LocalTime.parse("10:00");
+        LocalTime teenClock = LocalTime.parse("10:00")
+                .plus(12, ChronoUnit.HOURS);
         System.out.println(teenClock.toString());
+
+        int eight = LocalTime.parse("20:21").getHour();
+        System.out.println(eight);
+
+        //ZoneId zoneId = ZoneId.of("Europa/Madrit");
+
+        Set<String > allZones = ZoneId.getAvailableZoneIds();
+        allZones.forEach(System.out::println);
 
     }
 }
